3a0c7ae31ce6bdb4c62f018c15ad2fe5eca83d42
dreycat <dreycat@yandex.ru>
2021-01-21 17:11:18 +0500

    fix: test data

--- tests/__data/messages.ts
+++ tests/__data/messages.ts
@@ -1,6 +1,10 @@
+import config from '../../src/config';
+
+const { channel } = config.twitch;
+
 const joinAndPartMessage =
-  ':anotherttvviewer!anotherttvviewer@anotherttvviewer.tmi.twitch.tv JOIN #anthiveio\r\n' +
-  ':violets_tv!violets_tv@violets_tv.tmi.twitch.tv JOIN #anthiveio\r\n' +
-  ':nuclearpigeons!nuclearpigeons@nuclearpigeons.tmi.twitch.tv PART #anthiveio';
+  `:anotherttvviewer!anotherttvviewer@anotherttvviewer.tmi.twitch.tv JOIN #${channel}\r\n` +
+  `:violets_tv!violets_tv@violets_tv.tmi.twitch.tv JOIN #${channel}\r\n` +
+  `:nuclearpigeons!nuclearpigeons@nuclearpigeons.tmi.twitch.tv PART #${channel}`;
 
 export { joinAndPartMessage };
